f612053730fc649d3bb1e7b89383158163ef20ac
GWill44 <georgezygmund@hotmail.com>
2020-07-13 20:23:25 +0100

    Separation of the balance calculation for easier reading.

--- src/com/learningtocode/PaymentSchedule.java
+++ src/com/learningtocode/PaymentSchedule.java
@@ -4,7 +4,8 @@ public class PaymentSchedule {
     private static short totalNumberOfPayments = (short) (Console.getPeriod() * Mortgage.MONTHS_IN_YEAR);
     private static double calculateBalance(short numberOfPaymentsMade) {
         float monthlyInterest = Console.getAnnualInterest() / (Mortgage.MONTHS_IN_YEAR * Mortgage.PERCENT);
-        double balance = Console.getPrincipal() * ((Math.pow(1 + monthlyInterest, totalNumberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade)) / (Math.pow(1 + monthlyInterest, totalNumberOfPayments) - 1));
+        double balance =
+                Console.getPrincipal() * ((Math.pow(1 + monthlyInterest, totalNumberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade)) / (Math.pow(1 + monthlyInterest, totalNumberOfPayments) - 1));
         return balance;
     }
     public static void printPaymentSchedule() {
